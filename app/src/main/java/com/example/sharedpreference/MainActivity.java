a826cde6788aec64b9f56481090a3a0065cbc607
mj <farfs.dev@gmail.com>
2017-05-07 19:43:57 +0900

    add json example

--- app/src/main/java/com/example/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sharedpreference;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private static final String SETTINGS_PLAYER = "settings_player";
-    private static final String SETTINGS_ITEM_SPEED = "settings_item_speed";
-    private static final String SETTINGS_ITEM_VOLUME = "settings_item_volume";
-    private static final String SETTINGS_ITEM_LANGUAGE = "settings_item_language";
+    private static final String SETTINGS_PLAYER_SPEED = "settings_item_speed";
+    private static final String SETTINGS_PLAYER_VOLUME = "settings_item_volume";
+    private static final String SETTINGS_PLAYER_LANGUAGE = "settings_item_language";
+
+    private static final String SETTINGS_PLAYER_JSON = "settings_item_json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         btnPutSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                putSettingItem(SETTINGS_ITEM_SPEED, "fast");
-                putSettingItem(SETTINGS_ITEM_VOLUME, "20");
-                putSettingItem(SETTINGS_ITEM_LANGUAGE, "KOR");
+                putSettingItem(SETTINGS_PLAYER_SPEED, "fast");
+                putSettingItem(SETTINGS_PLAYER_VOLUME, "20");
+                putSettingItem(SETTINGS_PLAYER_LANGUAGE, "KOR");
             }
         });
 
@@ -34,9 +42,9 @@ public class MainActivity extends AppCompatActivity {
         btnGetSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String speed = getSettingItem(SETTINGS_ITEM_SPEED);
-                String volume = getSettingItem(SETTINGS_ITEM_VOLUME);
-                String lang = getSettingItem(SETTINGS_ITEM_LANGUAGE);
+                String speed = getSettingItem(SETTINGS_PLAYER_SPEED);
+                String volume = getSettingItem(SETTINGS_PLAYER_VOLUME);
+                String lang = getSettingItem(SETTINGS_PLAYER_LANGUAGE);
                 Log.d(TAG, "Speed: " + speed + ", Volume: "+volume + ", Language: " + lang);
             }
         });
@@ -45,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
         btnRemoveSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                removeSettingItem(SETTINGS_ITEM_SPEED);
-                removeSettingItem(SETTINGS_ITEM_VOLUME);
-                removeSettingItem(SETTINGS_ITEM_LANGUAGE);
+                removeSettingItem(SETTINGS_PLAYER_SPEED);
+                removeSettingItem(SETTINGS_PLAYER_VOLUME);
+                removeSettingItem(SETTINGS_PLAYER_LANGUAGE);
             }
         });
 
@@ -59,6 +67,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btnPutJson = (Button) findViewById(R.id.btn_put_json);
+        btnPutJson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> list = new ArrayList<String>();
+                list.add("first");
+                list.add("second");
+                list.add("third");
+                list.add("fourth");
+                setStringArrayPref(SETTINGS_PLAYER_JSON, list);
+                Log.d(TAG, "Put json");
+            }
+        });
+
+        Button btnGetJson = (Button) findViewById(R.id.btn_get_json);
+        btnGetJson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> list = getStringArrayPref(SETTINGS_PLAYER_JSON);
+                if (list != null) {
+                    for (String value : list) {
+                        Log.d(TAG, "Get json : " + value);
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -66,24 +101,24 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
-    private void putSettingItem(String item, String value) {
-        Log.d(TAG, "Put " + item +" (value : " + value + " ) to " + SETTINGS_PLAYER);
+    private void putSettingItem(String key, String value) {
+        Log.d(TAG, "Put " + key +" (value : " + value + " ) to " + SETTINGS_PLAYER);
         SharedPreferences preferences = getSharedPreferences(SETTINGS_PLAYER, MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(item, value);
+        editor.putString(key, value);
         editor.apply();
     }
 
-    private String getSettingItem(String item) {
-        Log.d(TAG, "Get " + item + " from " + SETTINGS_PLAYER);
-        return getSharedPreferences(SETTINGS_PLAYER, 0).getString(item, null);
+    private String getSettingItem(String key) {
+        Log.d(TAG, "Get " + key + " from " + SETTINGS_PLAYER);
+        return getSharedPreferences(SETTINGS_PLAYER, 0).getString(key, null);
     }
 
-    private void removeSettingItem(String item) {
-        Log.d(TAG, "Remove " + item + " from " + SETTINGS_PLAYER);
+    private void removeSettingItem(String key) {
+        Log.d(TAG, "Remove " + key + " from " + SETTINGS_PLAYER);
         SharedPreferences preferences = getSharedPreferences(SETTINGS_PLAYER, MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.remove(item);
+        editor.remove(key);
         editor.apply();
     }
 
@@ -94,4 +129,39 @@ public class MainActivity extends AppCompatActivity {
         editor.clear();
         editor.apply();
     }
+
+    public void setStringArrayPref(String key, ArrayList<String> values) {
+        SharedPreferences prefs = getSharedPreferences(SETTINGS_PLAYER, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) {
+            editor.putString(key, a.toString());
+        } else {
+            editor.putString(key, null);
+        }
+        editor.commit();
+    }
+
+    public ArrayList<String> getStringArrayPref(String key) {
+        SharedPreferences prefs = getSharedPreferences(SETTINGS_PLAYER, MODE_PRIVATE);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try {
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
+    }
+
+
 }
